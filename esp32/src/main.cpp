e386c12ecdf787c4862e6896edde1568bf4dd8b2
Olivier Nicol <olivier.nicol@corp.ovh.com>
2022-05-12 12:37:35 +0200

    press button and light led

--- esp32/src/main.cpp
+++ esp32/src/main.cpp
@@ -1,12 +1,32 @@
 #include <Arduino.h>
 
+const int buttonPin = 2;     // the number of the pushbutton pin
+const int ledPin =  15;      // the number of the LED pin
+int buttonState = 0;         // variable for reading the pushbutton status
+
 void setup() {
-  pinMode(15, OUTPUT);
+  // initialize the LED pin as an output:
+  pinMode(ledPin, OUTPUT);
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT);
+  Serial.begin(9600);
+  while (!Serial) {
+    ;  // wait for serial port to connect. Needed for native USB port only
+  }
+  Serial.println("setup");
 }
 
 void loop() {
-  digitalWrite(15, HIGH);
-  delay(100);
-  digitalWrite(15, LOW);
-  delay(900);
+  // read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);
+  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+  if (buttonState == HIGH) {
+    // turn LED on:
+    digitalWrite(ledPin, HIGH);
+    Serial.println("hey ho");
+    delay(2000);
+  } else {
+    // turn LED off:
+    digitalWrite(ledPin, LOW);
+  }
 }
